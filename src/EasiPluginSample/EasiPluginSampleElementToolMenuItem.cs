b13bc03489ea1c865a48197ee6721f3f8ad7b08b
lindexi <lindexi_gd@163.com>
2023-03-22 09:28:40 +0800

    加上修改元素坐标示例

--- src/EasiPluginSample/EasiPluginSampleElementToolMenuItem.cs
+++ src/EasiPluginSample/EasiPluginSampleElementToolMenuItem.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
 using Cvte.EasiNote;
 using Cvte.Paint.Features.Elements;
 using Cvte.Paint.Features.Elements.Texts;
@@ -56,6 +57,7 @@ namespace EasiPluginSample
                 // 文本有两个坐标系，一个是字符坐标，一个是光标坐标
                 // 文本坐标系是通用的文本知识，还请自行了解。遍历整个文本的所有字符，获取每个字符的范围，可使用如下代码
                 var charCount = firstTextElement.TextEditor.CharCount;
+                Point newTextElementPosition = default;
                 for (int i = 0; i < charCount; i++)
                 {
                     // 获取每个字符相对于文本元素的坐标
@@ -63,7 +65,14 @@ namespace EasiPluginSample
 
                     // 通过 WPF 的坐标系转换方法可以转换为页面坐标系
                     var charTopLeftInSlide = firstTextElement.TextEditor.TranslatePoint(bounds.TopLeft,currentSlide);
+
+                    // 在页面坐标系基础上，修改当前元素的坐标
+                    // 先使用有趣的算法算出新的坐标，以下代码是随便写的算法
+                    newTextElementPosition = charTopLeftInSlide with {X = (charTopLeftInSlide.X + newTextElementPosition.X)/2 };
                 }
+
+                // 设置当前元素的新范围
+                firstTextElement.Bounds = new Rect(newTextElementPosition, firstTextElement.Bounds.Size);
             }
 
             foreach (var groupElement in currentSlide.Elements.OfType<GroupElement>())
